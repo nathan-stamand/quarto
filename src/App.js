6f9c0c0a5ea658126e8f909ec8ffca86ba666fc3
Nathan St.Amand <nathansaint810@gmail.com>
2022-06-20 13:44:41 -0500

    Basic react setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+
+const App = () => {
+  return (
+    <StrictMode>
+      <div className="app">Here's your rendered app.</div>
+    </StrictMode>
+  );
+};
+
+export default App;
